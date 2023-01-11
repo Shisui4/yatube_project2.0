3dfab4362f78bd0bd988d881ee0cc9d95b009bd9
Серёжа <shisui4@mail.ru>
2023-01-11 19:35:08 +1000

    Added CSS and Images in templates page.

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,8 +3,19 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Главная страница записулек')
+    template = 'posts/index.html'
+    text = 'Это главная страница проекта Yatube'
+    context = {
+        'text': text
+    }
+    return render(request, template, context)
 
 
 def group_posts(request, slug):
-    return HttpResponse(f'Страница с {slug}')
+    template = 'posts/group_list.html'
+    text = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'text': text
+
+    }
+    return render(request, template, context)
